4377a30ff6f17a8adcbf153eb1df7dca55f3b6b5
troylelandshields <troy.leland.shields@gmail.com>
2021-07-01 15:54:15 -0600

    change start script so it doesn't require write-access to a file-system

--- docker/start-temporal.sh
+++ docker/start-temporal.sh
@@ -6,7 +6,7 @@ dockerize -template /etc/temporal/config/config_template.yaml:/etc/temporal/conf
 
 # Convert semicolon (or comma for backward compatibility) separated string (i.e. "history:matching")
 # to valid flags list (i.e. "--service=history --service=matching").
-IFS=':,' read -ra SERVICE_LIST <<< "$SERVICES"
+IFS=':,'; declare -a SERVICE_LIST=($SERVICES)
 SERVICE_ARGS=$(printf -- "--service=%s " "${SERVICE_LIST[@]}")
-
+SERVICE_ARGS="${SERVICE_ARGS%% }"
 exec temporal-server --root $TEMPORAL_HOME --env docker start $SERVICE_ARGS
